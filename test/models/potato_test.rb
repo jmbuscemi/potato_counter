846f5619ae42642d4251c345e5cd3775ec807408
John M Buscemi <jmbuscemi@gmail.com>
2015-10-30 08:43:42 -0400

    Test potato count.

--- test/models/potato_test.rb
+++ test/models/potato_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PotatoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "potato count" do
+    potato = potatoes(:one)
+    assert_equal 38, potato.count
+  end
 end
